edaf51fdbb3c1fd16de2133df1aca7d15c968e4a
Hishita Gupta <159264060+HishitaGupta@users.noreply.github.com>
2024-06-20 17:12:03 +0530

    let,const,char

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const id=900;
+// const id=900;
+// id=700; cant redeclare reassign value to constant variable 
+let email="hishita2gmail.com"
+// let email="hishita2gmail.com" -not possible to redeclare but value can be updated
+var pass="345";
+var pass="45"; //Redeclaration and updation of var possible
+city="Jiapur"
+let state;
+
+console.table([id,email,pass,city,state])
