b8fc05d40f089b5a2e273e8d167f9d5605f4fb2e
Hishita Gupta <159264060+HishitaGupta@users.noreply.github.com>
2024-06-20 17:12:35 +0530

    Primitive Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict";
+//treats all JS code as new version
+
+//alert(3+3) -> we are using node JS, not browser ,JS ka engine browser mein chupa hua hai
+
+//Primitive Data Types
+
+/*
+1. Number->1,2,3
+2. BigInt->595987980970
+3.String->"Hishita"
+4.Boolean->"True/False"
+5.Undefined->means not defined
+//typeof(undefined)->undefined
+6 Null->means empty
+ //typeof(null)->object
+7.Symbol ->used for uniqueness Figma etc.
+*/
