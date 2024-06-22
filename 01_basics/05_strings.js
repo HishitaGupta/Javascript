89e15f9bdf2acea95fb42b9338b51e7620ba112e
Hishita Gupta <159264060+HishitaGupta@users.noreply.github.com>
2024-06-22 22:30:44 +0530

    stack and heap

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,16 @@
+//String Interpolation
+let name="Hishita";
+let repoCount=60;
+console.log(`My name is ${name} and my repo count is ${repoCount}.` );
+
+//Ways to Declare String
+let school="mukat"
+const gameName= new String('hishitag')
+
+//String is an Object , Comes with various methods
+console.log(gameName[0])
+console.log(gameName.__proto__) //to get the object -{}-can be viewed with values in console
+
+//Methods
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
